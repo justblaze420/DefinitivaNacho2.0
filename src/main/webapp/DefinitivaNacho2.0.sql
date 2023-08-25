081b58dd5f7026ea4d4b448600ff9ca3b4de3df7
Robbie Daniel <132608099+justblaze420@users.noreply.github.com>
2023-08-25 00:28:48 -0600

    DefinitivaNacho2.0.sql
    Script

--- src/main/webapp/DefinitivaNacho2.0.sql
+++ src/main/webapp/DefinitivaNacho2.0.sql
@@ -27,31 +27,12 @@ CREATE TABLE Registros(
     FOREIGN KEY (idPersonal) REFERENCES personal(idPersonal)
 );
 
-select * from Registros;
-
-select * from departamento;
-
-select * from personal;
-
-SELECT r.idRegistro, r.idPersonal, r.horaEntrada, r.horaSalida, p.nombre, p.apellido, TIME_FORMAT(TIMEDIFF(r.horaSalida, r.horaEntrada), '%H:%i:%s') AS duracion
-                            FROM registros r 
-                            JOIN personal p ON r.idPersonal = p.idPersonal
-INSERT INTO personal (nombre, idDepartamento) VALUES ('Robbie Daniel', 2);
-
-delete from registros where idRegistro = 53;
-
-
-INSERT INTO registros (idRegistro, idPersonal, horaEntrada, horaSalida)
-VALUES (53, 23, '2023-08-14 12:50:41.0', '2023-08-16 12:51:53.0');
-
 
 
 
 
 ALTER TABLE Registros DROP FOREIGN KEY registros_ibfk_1;
 ALTER TABLE Registros ADD CONSTRAINT registros_ibfk_1 FOREIGN KEY (idPersonal) REFERENCES personal(idPersonal) ON DELETE CASCADE;
-
-SHOW CREATE TABLE Registros;
 
 
 INSERT INTO departamento (nombre) VALUES ('Profesores');
@@ -62,10 +43,6 @@ INSERT INTO departamento (nombre) VALUES ('Administracion');
 INSERT INTO departamento (nombre) VALUES ('Psiquiatria');
 INSERT INTO departamento (nombre) VALUES ('Areas Sociales');
 
-ALTER TABLE personal DROP FOREIGN KEY personal_ibfk_1;
-UPDATE personal SET idDepartamento = 1 WHERE idPersonal = 36;
-
-SELECT personal.*, departamento.nombre AS nombreDepartamento FROM personal JOIN departamento ON personal.idDepartamento = departamento.idDepartamento
 
 
 SELECT 
@@ -87,12 +64,6 @@ JOIN
 ORDER BY fecha
 
 
-SELECT p.idPersonal, p.nombre, p.apellido, DATE(r.horaEntrada) as fecha, 
-       SUM(TIMESTAMPDIFF(HOUR, r.horaEntrada, r.horaSalida)) as totalHoras 
-FROM registros r
-JOIN personal p ON r.idPersonal = p.idPersonal
-WHERE r.horaEntrada BETWEEN '2023-08-01' AND '2023-08-31'
-GROUP BY p.idPersonal, DATE(r.horaEntrada);
 
 
 
